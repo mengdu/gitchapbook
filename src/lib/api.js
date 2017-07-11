baccbcf8391d0dbd728924b293283f521ee8d79c
mengdu <1752295326@qq.com>
2017-07-11 23:12:47 +0800

    update

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,10 +3,11 @@ const ROOT = "https://mengdu.github.io/website"
 
 const DBS = "dbs.json"
 const BANNER = "banner.json"
+const ARTICLEDB = "dbs"
+
 
 export const WEBROOT = ROOT
 export const ArticleListDbPath = ROOT + '/' + DBS
-
 export const BannerDbPath = ROOT + '/' + BANNER
-
 export const IMGSDIR = ROOT + '/' +"imgs"
+
