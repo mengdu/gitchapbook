09cf11be4ed18c7a67fb5d353639a0ebd93aa14b
lyg <ygl@szmsmk.com.cn>
2017-07-11 19:29:38 +0800

    update

---
+++ src/lib/api.js
@@ -0,0 +1,12 @@
+
+const ROOT = "https://mengdu.github.io/website"
+
+const DBS = "dbs.json"
+const BANNER = "banner.json"
+
+export const WEBROOT = ROOT
+export const ArticleListDbPath = ROOT + '/' + DBS
+
+export const BannerDbPath = ROOT + '/' + BANNER
+
+export const IMGSDIR = ROOT + '/' +"imgs"
